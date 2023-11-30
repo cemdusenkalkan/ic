b92aefaf98342a3e6ffe5e403ef94a1b796ac4da
cemd <99793829+cemdusenkalkan@users.noreply.github.com>
2023-11-30 14:48:48 +0300

    Update compressor.cpp
    prototype

--- compressor.cpp
+++ compressor.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include <string>
 
+using namespace std;
+
+
+bool readPGM(const char* filename, unsigned char*& imageData, size_t& width, size_t& height, size_t& maxVal);
+bool writePGM(const char* filename, const unsigned char* imageData, size_t width, size_t height, size_t maxVal);
+void compressImage(const unsigned char* inputImageData, size_t width, size_t height, unsigned char*& outputImageData, size_t& outputSize);
+unsigned char* resizeImage(const unsigned char* inputImageData, size_t currentWidth, size_t currentHeight, size_t newWidth, size_t newHeight);
+void deallocateMemory(unsigned char*& imageData);
